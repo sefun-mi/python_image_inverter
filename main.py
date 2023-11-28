1a5bdf77a389dde79e2407a711c663344efb0e17
sefunmi <iv.vi.kyle@gmail.com>
2023-11-29 00:12:23 +0100

    update

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from PIL import Image
 
-image_list = []
-file_name ="eren.jpg"                         #input("enter file path to image here: ")
+                                              #image_list = []
+file_name =input("enter file path to image here: ")
 img_obj = Image.open(file_name)
+pixel_map = img_obj.load()
 length, breadth = img_obj.size
 
-for i in range(0,length):
-    for j in range(0,breadth):
+for i in range(length):
+    for j in range(breadth):
+        r, g, b = img_obj.getpixel((i,j))
+        pixel_map[i,j] = (255-r, 255-g, 255-b)
 
-        
-image_list.append(file_name)
-print(image_list[0])
+
+img_obj.save("output", format="png")
+                                                #image_list.append(file_name)
+                                                #print(image_list[0])
