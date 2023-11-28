cda2930cd2a44a1ad370d376634395542b1bb482
sefunmi <iv.vi.kyle@gmail.com>
2023-11-28 23:59:01 +0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-#import imageio as iio
+from PIL import Image
 
 image_list = []
-file_name = input("enter file path to image here: ")
+file_name ="eren.jpg"                         #input("enter file path to image here: ")
+img_obj = Image.open(file_name)
+length, breadth = img_obj.size
+
+for i in range(0,length):
+    for j in range(0,breadth):
+
+        
 image_list.append(file_name)
 print(image_list[0])
